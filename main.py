3854d6d18685b47001eae09d7d552724360b6c90
calliouxes <92147165+EliseeLeydier@users.noreply.github.com>
2022-02-18 15:11:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,36 +35,38 @@ dic = {0 : [False, False, +1, +3],
        7 : [-3, -1, +1, False],
        8 : [-3, -1, False, False]}
 
-initial = [8,1,3,4,0,2,7,6,5]
-vus = []
+initial = [8,1,3,4,0,2,7,6,5] # plateau de base
+vus = [] # Liste de tous les états déja vus (pour évité les répétitions)
 
 for i in range(20):
 #while(calculateManhattan(initial) > 0):
 
-    vus.append(copy.deepcopy(initial))
+    vus.append(copy.deepcopy(initial)) #
     afficherBien(initial)
     print()
 
-    listePlateau = [initial, copy.deepcopy(initial), copy.deepcopy(initial), copy.deepcopy(initial), copy.deepcopy(initial)]
+    listePlateau = [initial, copy.deepcopy(initial), copy.deepcopy(initial), copy.deepcopy(initial), copy.deepcopy(initial)]  
 
+    #On cherche l'emplacement vide
     for i in range(len(initial)):
         if initial[i] == 0:
             index = i
 
-    liste = dic[index]
     #print(listePlateau)
 
+    #on met toute les possibilité de jeu dans ListePlateau (enfonction de "dic")
     for i in range(1, 5):
         j = i-1
-        if liste:
+        if dic[index]:
             #print(listePlateau[i], "     ", index)
             #print(listePlateau[i][index+(liste[j])])
-            listePlateau[i][index],listePlateau[i][index+(liste[j])] = echanger(listePlateau[i][index], listePlateau[i][index+(liste[j])])
+            listePlateau[i][index],listePlateau[i][index+(dic[index][j])] = echanger(listePlateau[i][index], listePlateau[i][index+(dic[index][j])])
         else:
             listePlateau[i] = False
 
 
 
+    # On cherche quelle est la + petite distance de Manhatta
     min = 100
     for i in range(1,5):
         #print("min:", min, "listePlateau[i] : ", listePlateau[i])
